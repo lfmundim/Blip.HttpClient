f01a0eb10100c343c74b13b0af8a5c7221232420
Lucas Mundim <lucasm@take.net>
2019-01-21 19:10:23 -0200

    added default IBroadcastExtension methods to BroadcastService and declared the overloads in the IBroadcastService file

--- Blip.HttpClient/Services/BroadcastService.cs
+++ Blip.HttpClient/Services/BroadcastService.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 using Blip.HttpClient.Exceptions;
 using Blip.HttpClient.Factories;
 using Lime.Protocol;
+using Serilog;
 using Take.Blip.Client;
 using Take.Blip.Client.Extensions.Broadcast;
 
 namespace Blip.HttpClient.Services
 {
-    public class BroadcastService : IBroadcastService, IBroadcastExtension
+    public class BroadcastService : IBroadcastService
     {
         private readonly ISender _sender;
 
@@ -30,55 +31,164 @@ namespace Blip.HttpClient.Services
         {
             try
             {
-                var command = 
-            }
-            catch (BlipHttpClientException bex)
-            {
-
+                var broadcastExtension = new BroadcastExtension(_sender);
+                await broadcastExtension.AddRecipientAsync(listName, recipientIdentity, cancellationToken);
             }
             catch (Exception ex)
             {
-
-                throw;
+                throw ex;
             }
         }
 
-        public Task CreateDistributionListAsync(string listName, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task AddRecipientAsync(string listName, Identity recipientIdentity, CancellationToken cancellationToken, ILogger logger)
         {
             throw new NotImplementedException();
         }
 
-        public Task DeleteDistributionListAsync(string listName, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task CreateDistributionListAsync(string listName, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                var broadcastExtension = new BroadcastExtension(_sender);
+                await broadcastExtension.CreateDistributionListAsync(listName, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task CreateDistributionListAsync(string listName, CancellationToken cancellationToken, ILogger logger)
         {
             throw new NotImplementedException();
         }
 
-        public Task DeleteRecipientAsync(string listName, Identity recipientIdentity, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task DeleteDistributionListAsync(string listName, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                var broadcastExtension = new BroadcastExtension(_sender);
+                await broadcastExtension.DeleteDistributionListAsync(listName, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task DeleteDistributionListAsync(string listName, CancellationToken cancellationToken, ILogger logger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteRecipientAsync(string listName, Identity recipientIdentity, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                var broadcastExtension = new BroadcastExtension(_sender);
+                await broadcastExtension.DeleteRecipientAsync(listName, recipientIdentity, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task DeleteRecipientAsync(string listName, Identity recipientIdentity, CancellationToken cancellationToken, ILogger logger)
         {
             throw new NotImplementedException();
         }
 
         public Identity GetListIdentity(string listName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var broadcastExtension = new BroadcastExtension(_sender);
+                var listIdentity = broadcastExtension.GetListIdentity(listName);
+                return listIdentity;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
-        public Task<DocumentCollection> GetRecipientsAsync(string listName, int skip = 0, int take = 100, CancellationToken cancellationToken = default(CancellationToken))
+        public Identity GetListIdentity(string listName, ILogger logger)
         {
             throw new NotImplementedException();
         }
 
-        public Task<DocumentCollection> GetRecipientsAsynGetAllDistributionListsAsync(int skip = 0, int take = 100, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<DocumentCollection> GetRecipientsAsync(string listName, int skip = 0, int take = 100, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                var broadcastExtension = new BroadcastExtension(_sender);
+                var recipients = await broadcastExtension.GetRecipientsAsync(listName, skip, take, cancellationToken);
+                return recipients;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<DocumentCollection> GetRecipientsAsync(ILogger logger, CancellationToken cancellationToken, string listName, int skip = 0, int take = 100)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> HasRecipientAsync(string listName, Identity recipientIdentity, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<DocumentCollection> GetRecipientsAsynGetAllDistributionListsAsync(int skip = 0, int take = 100, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                var broadcastExtension = new BroadcastExtension(_sender);
+                var allDistributionListsRecipients = await broadcastExtension.GetRecipientsAsynGetAllDistributionListsAsync(skip, take, cancellationToken);
+                return allDistributionListsRecipients;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<DocumentCollection> GetRecipientsAsynGetAllDistributionListsAsync(ILogger logger, CancellationToken cancellationToken, int skip = 0, int take = 100)
         {
             throw new NotImplementedException();
         }
 
-        public Task SendMessageAsync(string listName, Document content, string id = null, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<bool> HasRecipientAsync(string listName, Identity recipientIdentity, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                var broadcastExtension = new BroadcastExtension(_sender);
+                var hasRecipient = await broadcastExtension.HasRecipientAsync(listName, recipientIdentity, cancellationToken);
+                return hasRecipient;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<bool> HasRecipientAsync(string listName, Identity recipientIdentity, CancellationToken cancellationToken, ILogger logger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SendMessageAsync(string listName, Document content, string id = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            try
+            {
+                var broadcastExtension = new BroadcastExtension(_sender);
+                await broadcastExtension.SendMessageAsync(listName, content, id, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task SendMessageAsync(ILogger logger, CancellationToken cancellationToken, string listName, Document content, string id = null)
         {
             throw new NotImplementedException();
         }
