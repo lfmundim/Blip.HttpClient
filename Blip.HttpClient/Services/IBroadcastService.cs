07dd0f7f009f187125425084d256d1089273d10f
lfmundim <lucasfmundim@ufmg.br>
2019-01-22 22:11:29 -0200

    re-added broadcastservice base stuff

---
+++ Blip.HttpClient/Services/IBroadcastService.cs
@@ -0,0 +1,32 @@
+﻿using Lime.Protocol;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Take.Blip.Client.Extensions.Broadcast;
+
+namespace Blip.HttpClient.Services
+{
+    public interface IBroadcastService : IBroadcastExtension
+    {
+        Task AddRecipientAsync(string listName, Identity recipientIdentity, CancellationToken cancellationToken, ILogger logger);
+
+        Task CreateDistributionListAsync(string listName, CancellationToken cancellationToken, ILogger logger);
+
+        Task DeleteDistributionListAsync(string listName, CancellationToken cancellationToken, ILogger logger);
+
+        Task DeleteRecipientAsync(string listName, Identity recipientIdentity, CancellationToken cancellationToken, ILogger logger);
+
+        Identity GetListIdentity(string listName, ILogger logger);
+
+        Task<DocumentCollection> GetRecipientsAsync(ILogger logger, CancellationToken cancellationToken, string listName, int skip = 0, int take = 100);
+
+        Task<DocumentCollection> GetRecipientsAsynGetAllDistributionListsAsync(ILogger logger, CancellationToken cancellationToken, int skip = 0, int take = 100);
+
+        Task<bool> HasRecipientAsync(string listName, Identity recipientIdentity, CancellationToken cancellationToken, ILogger logger);
+
+        Task SendMessageAsync(ILogger logger, CancellationToken cancellationToken, string listName, Document content, string id = null);
+    }
+}
